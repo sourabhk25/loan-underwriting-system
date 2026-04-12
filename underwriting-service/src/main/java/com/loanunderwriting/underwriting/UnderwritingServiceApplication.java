55acb174ed040372ceb2b0d4809f50f11d87d863
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2026-04-12 16:58:06 -0500

    Add initial code for all services

---
+++ underwriting-service/src/main/java/com/loanunderwriting/underwriting/UnderwritingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.loanunderwriting.underwriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderwritingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderwritingServiceApplication.class, args);
+	}
+
+}
---
+++ underwriting-service/src/test/java/com/loanunderwriting/underwriting/UnderwritingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanunderwriting.underwriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderwritingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
